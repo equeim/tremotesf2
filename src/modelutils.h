2cee5a2364e4fb6f05fd9adc492b1f5931233ba4
Alexey Rochev <equeim@gmail.com>
2021-10-31 00:27:22 +0300

    Add ModelListUpdater

--- src/modelutils.h
+++ src/modelutils.h
@@ -21,6 +21,8 @@
 
 #include <QAbstractItemModel>
 
+#include "libtremotesf/itemlistupdater.h"
+
 namespace tremotesf
 {
     class ModelBatchChanger
@@ -111,6 +113,40 @@ namespace tremotesf
 
         int firstRow = -1;
         int lastRow = -1;
+    };
+
+    template<typename Model, typename Item, typename NewItem = Item, typename NewItemContainer = std::vector<NewItem>>
+    class ModelListUpdater : public ItemListUpdater<Item, NewItem, NewItemContainer> {
+        static_assert(std::is_base_of_v<QAbstractItemModel, Model>);
+
+    public:
+        inline explicit ModelListUpdater(Model& model) : mModel(model) {}
+
+    protected:
+        void onChangedItems(size_t first, size_t last) override {
+            emit mModel.dataChanged(mModel.index(static_cast<int>(first), lastColumn), mModel.index(static_cast<int>(last - 1), lastColumn));
+        }
+
+        void onAboutToRemoveItems(size_t first, size_t last) override {
+            mModel.beginRemoveRows({}, static_cast<int>(first), static_cast<int>(last - 1));
+        };
+
+        void onRemovedItems(size_t, size_t) override {
+            mModel.endRemoveRows();
+        }
+
+        void onAboutToAddItems(size_t count) override {
+            const int first = mModel.rowCount();
+            mModel.beginInsertRows({}, first, first + static_cast<int>(count) - 1);
+        }
+
+        void onAddedItems(size_t) override {
+            mModel.endInsertRows();
+        };
+
+    protected:
+        Model& mModel;
+        const int lastColumn = static_cast<QAbstractItemModel&>(mModel).columnCount() - 1;
     };
 }
 
