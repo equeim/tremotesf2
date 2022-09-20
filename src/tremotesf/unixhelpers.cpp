8ecded912111e2edb0b1e66bb2933c2ccd847459
Alexey Rochev <equeim@gmail.com>
2022-09-20 22:04:13 +0300

    Simplify checkPosixError

--- src/tremotesf/unixhelpers.cpp
+++ src/tremotesf/unixhelpers.cpp
@@ -1,15 +1,12 @@
 #include "unixhelpers.h"
 
 #include <cerrno>
-#include <stdexcept>
-
-#include <fmt/format.h>
+#include <system_error>
 
 #include "libtremotesf/log.h"
 
 namespace tremotesf::impl {
-    int64_t checkPosixError(int64_t result, std::string_view functionName) {
-        if (result != -1) return result;
+    void throwWithErrno(std::string_view functionName) {
         const auto baseError = std::system_error(errno, std::system_category());
         throw std::system_error(
             baseError.code(),
