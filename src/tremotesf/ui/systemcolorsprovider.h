fdc7948df7c41387e62e6d47342201fc6991bd41
Alexey Rochev <equeim@gmail.com>
2022-10-20 01:14:45 +0300

    Don't use trailing return type in fmt::formatter specializations
    It seems that clang-format doesn't like FORMAT_CONST macro
    together with trailing return type. Use regular function declaration
    to make formatted source a bit more readable.

--- src/tremotesf/ui/systemcolorsprovider.h
+++ src/tremotesf/ui/systemcolorsprovider.h
@@ -53,8 +53,7 @@ SPECIALIZE_FORMATTER_FOR_QDEBUG(QColor)
 
 template<>
 struct fmt::formatter<tremotesf::SystemColorsProvider::AccentColors> : libtremotesf::SimpleFormatter {
-    auto format(const tremotesf::SystemColorsProvider::AccentColors& colors, format_context& ctx)
-        FORMAT_CONST->decltype(ctx.out()) {
+    format_context::iterator format(const tremotesf::SystemColorsProvider::AccentColors& colors, format_context& ctx) FORMAT_CONST {
         return format_to(
             ctx.out(),
             "AccentColors(accentColor={}, accentColorLight1={})",
