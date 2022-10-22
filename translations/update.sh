6fafa9d201884153719d2574e3312ead65ccddbe
Alexey Rochev <equeim@gmail.com>
2022-10-22 16:50:47 +0300

    Update translations update script

--- translations/update.sh
+++ translations/update.sh
@@ -1,25 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 # SPDX-FileCopyrightText: 2015-2022 Alexey Rochev
 #
 # SPDX-License-Identifier: CC0-1.0
 
-if command -v lupdate-qt5 > /dev/null 2>&1; then
-    _lupdate=lupdate-qt5
-else
-    _lupdate=lupdate
-fi
-
 _dir="$(realpath $(dirname $0))"
 cd "$_dir"
 
-QT_SELECT=5 $_lupdate ../src ../qml -ts en.ts \
-                                        es.ts \
-                                        fr.ts \
-                                        it_IT.ts \
-                                        nl_BE.ts \
-                                        nl.ts \
-                                        pl.ts \
-                                        ru.ts \
-                                        zh_CN.ts \
-                                        source.ts
+lupdate-qt5 ../src/tremotesf -no-obsolete -ts en.ts \
+                                              es.ts \
+                                              fr.ts \
+                                              it_IT.ts \
+                                              nl_BE.ts \
+                                              nl.ts \
+                                              pl.ts \
+                                              ru.ts \
+                                              zh_CN.ts \
+                                              source.ts
