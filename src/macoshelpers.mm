cbb344a9955af96ba6d510c039f45111328399c5
Alexey Rochev <equeim@gmail.com>
2023-11-29 01:47:28 +0300

    Make macOS menu bar switch to previous application when we hide ourselves

---
+++ src/macoshelpers.mm
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2015-2023 Alexey Rochev
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "macoshelpers.h"
+
+#include <AppKit/NSApplication.h>
+
+namespace tremotesf {
+    void hideNSApp() { [NSApp hide:nullptr]; }
+}
