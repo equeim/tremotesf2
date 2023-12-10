000485c1f23b02cd2ded0cb245a14924fba01481
Alexey Rochev <equeim@gmail.com>
2023-12-11 00:17:59 +0300

    Build macOS dependencies with debug info

--- vcpkg-overlay-triplets/x64-osx-release.cmake
+++ vcpkg-overlay-triplets/x64-osx-release.cmake
@@ -5,3 +5,5 @@
 include("${VCPKG_ROOT_DIR}/triplets/community/x64-osx-release.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/../cmake/MacOSDeploymentTarget.cmake")
 set(VCPKG_OSX_DEPLOYMENT_TARGET "${TREMOTESF_MACOS_DEPLOYMENT_TARGET}")
+set(VCPKG_CXX_FLAGS -g)
+set(VCPKG_C_FLAGS -g)
