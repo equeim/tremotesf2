37efcc1f9b46f5ea00bdfb27385ca8fd73ddc5d2
Alexey Rochev <equeim@gmail.com>
2024-01-30 02:39:17 +0300

    Set WINVER and _WIN32_WINNT macros when building dependencies

---
+++ cmake/WindowsMinimumVersion.cmake
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2015-2024 Alexey Rochev
+#
+# SPDX-License-Identifier: CC0-1.0
+
+# Windows 10
+set(TREMOTESF_WINDOWS_WINVER_MACRO "0x0A00")
+set(TREMOTESF_WINDOWS_SUPPORTED_OS_ID "{8e0f7a12-bfb3-4fe8-b9a5-48fd50a15a9a}")
--- vcpkg-overlay-triplets/x64-windows-static.cmake
+++ vcpkg-overlay-triplets/x64-windows-static.cmake
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: CC0-1.0
 
 include("${VCPKG_ROOT_DIR}/triplets/x64-windows-static.cmake")
-# Always Use C++20 coroutines ABI
-set(VCPKG_CXX_FLAGS /await:strict)
-set(VCPKG_C_FLAGS /await:strict)
+include("${CMAKE_CURRENT_LIST_DIR}/../cmake/WindowsMinimumVersion.cmake")
+# /await:strict - use C++20 coroutines ABI when building C++17 dependencies
+set(flags "/await:strict /DWINVER=${TREMOTESF_WINDOWS_WINVER_MACRO} /D_WIN32_WINNT=${TREMOTESF_WINDOWS_WINVER_MACRO}")
+set(VCPKG_CXX_FLAGS "${flags}")
+set(VCPKG_C_FLAGS "${flags}")
