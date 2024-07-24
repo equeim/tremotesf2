5b7e3b1090a4a25b3f6faf9988bece69f74e04fa
Alexey Rochev <equeim@gmail.com>
2024-07-25 00:20:02 +0300

    Properly parse magnet link

---
+++ src/magnetlinkparser.cpp
@@ -0,0 +1,62 @@
+// SPDX-FileCopyrightText: 2015-2024 Alexey Rochev
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <ranges>
+#include <stdexcept>
+
+#include <QUrlQuery>
+
+#include <fmt/ranges.h>
+
+#include "magnetlinkparser.h"
+#include "stdutils.h"
+
+namespace tremotesf {
+    namespace {
+        constexpr auto xtKey = "xt"_l1;
+        constexpr auto xtValuePrefix = "urn:btih:"_l1;
+        constexpr auto trKey = "tr"_l1;
+    }
+
+    TorrentMagnetLink parseMagnetLink(const QUrl& url) {
+        if (url.scheme() != magnetScheme) {
+            throw std::runtime_error("URL scheme must be magnet");
+        }
+
+        const QUrlQuery query(url);
+
+        const auto xtValues = query.allQueryItemValues(xtKey, QUrl::FullyDecoded);
+        const auto infoHashV1Value =
+            std::ranges::find_if(xtValues, [](const auto& value) { return value.startsWith(xtValuePrefix); });
+        if (infoHashV1Value == xtValues.end()) {
+            throw std::runtime_error("Did not find v1 info hash in the URL");
+        }
+        auto infoHashV1 = infoHashV1Value->mid(xtValuePrefix.size()).toLower();
+
+        auto trackers = toContainer<std::vector>(
+            query.allQueryItemValues(trKey, QUrl::FullyDecoded) |
+            std::views::transform([](auto tracker) { return std::set{tracker}; })
+        );
+
+        return {.infoHashV1 = std::move(infoHashV1), .trackers = std::move(trackers)};
+    }
+
+    QDebug operator<<(QDebug debug, const TorrentMagnetLink& magnetLink) {
+        const QDebugStateSaver saver(debug);
+        debug.noquote() << fmt::format(impl::singleArgumentFormatString, magnetLink).c_str();
+        return debug;
+    }
+
+}
+
+fmt::format_context::iterator fmt::formatter<tremotesf::TorrentMagnetLink>::format(
+    const tremotesf::TorrentMagnetLink& magnetLink, format_context& ctx
+) const {
+    return fmt::format_to(
+        ctx.out(),
+        "MagnetLink(infoHashV1={}, trackers={})",
+        magnetLink.infoHashV1,
+        magnetLink.trackers
+    );
+}
---
+++ src/magnetlinkparser.h
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2015-2024 Alexey Rochev
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef TREMOTESF_MAGNETLINKPARSER_H
+#define TREMOTESF_MAGNETLINKPARSER_H
+
+#include <set>
+#include <vector>
+
+#include "log/formatters.h"
+#include "literals.h"
+
+class QUrl;
+
+namespace tremotesf {
+    inline constexpr auto magnetScheme = "magnet"_l1;
+
+    struct TorrentMagnetLink {
+        QString infoHashV1;
+        std::vector<std::set<QString>> trackers;
+
+        bool operator==(const TorrentMagnetLink&) const = default;
+    };
+
+    /**
+     * @throws std::runtime_error
+     */
+    TorrentMagnetLink parseMagnetLink(const QUrl& url);
+
+    QDebug operator<<(QDebug debug, const TorrentMagnetLink& magnetLink);
+}
+
+template<>
+struct fmt::formatter<tremotesf::TorrentMagnetLink> : tremotesf::SimpleFormatter {
+    format_context::iterator format(const tremotesf::TorrentMagnetLink& magnetLink, format_context& ctx) const;
+};
+
+#endif // TREMOTESF_MAGNETLINKPARSER_H
