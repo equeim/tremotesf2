b078584fcffbe447dc6f05c7316b976ca0f50205
Alexey Rochev <equeim@gmail.com>
2024-09-01 02:06:52 +0300

    Expose formatExceptionRecursively function
    Also log exception together with its associated message, not separately

--- src/log/log.cpp
+++ src/log/log.cpp
@@ -24,8 +24,54 @@ namespace tremotesf {
         qputenv(loggingRulesEnvVariable, rules);
     }
 
+    namespace {
+        template<typename T>
+        void appendNestedException(std::string& out, const T& e) {
+            fmt::format_to(std::back_insert_iterator(out), "\nCaused by: {}", e);
+        }
+
+        template<typename T>
+        void appendNestedExceptions(std::string& out, const T& e) {
+            try {
+                std::rethrow_if_nested(e);
+            } catch (const std::system_error& nested) {
+                appendNestedException(out, nested);
+                appendNestedExceptions(out, nested);
+            } catch (const std::exception& nested) {
+                appendNestedException(out, nested);
+                appendNestedExceptions(out, nested);
+            }
+#ifdef Q_OS_WIN
+            catch (const winrt::hresult_error& nested) {
+                appendNestedException(out, nested);
+                appendNestedExceptions(out, nested);
+            }
+#endif
+            catch (...) {
+                fmt::format_to(std::back_insert_iterator(out), "\nCaused by: unknown exception");
+            }
+        }
+    }
+
+    template<impl::IsException E>
+    std::string formatExceptionRecursivelyImpl(const E& e) {
+        std::string out = fmt::format(impl::singleArgumentFormatString, e);
+        appendNestedExceptions(out, e);
+        return out;
+    }
+
+    std::string formatExceptionRecursively(const std::exception& e) { return formatExceptionRecursivelyImpl(e); }
+    std::string formatExceptionRecursively(const std::system_error& e) { return formatExceptionRecursivelyImpl(e); }
+#ifdef Q_OS_WIN
+    std::string formatExceptionRecursively(const winrt::hresult_error& e) { return formatExceptionRecursivelyImpl(e); }
+#endif
+
+    QString Logger::formatToQString(fmt::string_view fmt, fmt::format_args args) {
+        return QString::fromStdString(fmt::vformat(fmt, args));
+    }
+
     void Logger::logWithFormatArgs(fmt::string_view fmt, fmt::format_args args) const {
-        logImpl(QString::fromStdString(fmt::vformat(fmt, args)));
+        logImpl(formatToQString(fmt, args));
     }
 
     void Logger::logImpl(const QString& string) const {
@@ -36,28 +82,4 @@ namespace tremotesf {
         // 2. QMessageLogger::<>(const char*, ...) overloads perform QString::vasprintf() formatting
         qt_message_output(type, context, string);
     }
-
-    template<impl::IsException E>
-    void Logger::logExceptionRecursively(const E& e) const {
-        logImpl(QString::fromStdString(fmt::format(" |- Caused by: {}", e)));
-        try {
-            std::rethrow_if_nested(e);
-        } catch (const std::exception& nested) {
-            logExceptionRecursively(nested);
-        }
-#ifdef Q_OS_WIN
-        catch (const winrt::hresult_error& nested) {
-            logExceptionRecursively(nested);
-        }
-#endif
-        catch (...) {
-            logImpl(QStringLiteral(" |- Caused by: unknown exception"));
-        }
-    }
-
-    template void Logger::logExceptionRecursively<std::exception>(const std::exception&) const;
-    template void Logger::logExceptionRecursively<std::system_error>(const std::system_error&) const;
-#ifdef Q_OS_WIN
-    template void Logger::logExceptionRecursively<winrt::hresult_error>(const winrt::hresult_error&) const;
-#endif
 }
