44258ef9fbbf80990f609ee2f8610f1f99e65360
Alexey Rochev <equeim@gmail.com>
2024-12-16 22:23:44 +0300

    Add Windows on ARM support

---
+++ vcpkg-overlay-triplets/arm64-windows-static.cmake
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2015-2024 Alexey Rochev
+#
+# SPDX-License-Identifier: CC0-1.0
+
+include("${VCPKG_ROOT_DIR}/triplets/community/arm64-windows-static.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/../cmake/WindowsMinimumVersion.cmake")
+# /await:strict - use C++20 coroutines ABI when building C++17 dependencies
+set(flags "/await:strict /DWINVER=${TREMOTESF_WINDOWS_WINVER_MACRO} /D_WIN32_WINNT=${TREMOTESF_WINDOWS_WINVER_MACRO}")
+set(VCPKG_CXX_FLAGS "${flags}")
+set(VCPKG_C_FLAGS "${flags}")
